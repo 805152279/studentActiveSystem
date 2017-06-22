938b42f14ceeb102bb2b7b97fde9c66c15009b24
ZYH <ZYH@administrator>
2017-06-22 13:39:58 +0800

    login

--- activityMangeSystem/src/test/Test.java
+++ activityMangeSystem/src/test/Test.java
@@ -1,5 +1,28 @@
 package test;
 
-public class Test {
+import hib.com.HibernateSessionFactory;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.bean.User;
+
+public class Test {
+public static void main(String[] args) {
+	Session session = HibernateSessionFactory.getSession();  
+    Transaction tx = session.beginTransaction();  
+    User stu = new User("2014", "∆§∆§∫¿", "1234",1,5,0);  
+   
+    try {  
+   session.save(stu);   //insert≤Â»Î
+    
+    
+        tx.commit();  
+    } catch (Exception e) {  
+        tx.rollback();  
+        e.printStackTrace();  
+    }finally{  
+        session.close();  
+    }  
+}
 }
